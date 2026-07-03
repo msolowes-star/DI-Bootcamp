283157f0181e7451ef23aef946c254166ce577c8
Marc <msolowes@gmail.com>
2026-07-03 09:11:32 +0300

    Complete Week 3 Day 2 exercises

--- Week3/Day2/ExerciseXP/Week3Day2ExerciseXP.py
+++ Week3/Day2/ExerciseXP/Week3Day2ExerciseXP.py
@@ -1,3 +1,6 @@
+from dog import Dog
+import random
+
 # Exercise 1: Pets
 
 class Pets():
@@ -43,41 +46,6 @@ all_cats = [bengal_cat, chartreux_cat, siamese_cat]
 sara_pets = Pets(all_cats)
 
 sara_pets.walk()
-
-# Exercise 2: Dogs
-
-class Dog:
-    def __init__(self, name, age, weight):
-        self.name = name
-        self.age = age
-        self.weight = weight
-
-    def bark(self):
-        return f"{self.name} is barking"
-
-    def run_speed(self):
-        return self.weight / self.age * 10
-
-    def fight(self, other_dog):
-        my_power = self.run_speed() * self.weight
-        other_power = other_dog.run_speed() * other_dog.weight
-
-        if my_power > other_power:
-            return f"{self.name} won the fight"
-        elif other_power > my_power:
-            return f"{other_dog.name} won the fight"
-        else:
-            return "It was a tie"
-
-
-dog1 = Dog("Rex", 4, 20)
-dog2 = Dog("Buddy", 3, 15)
-dog3 = Dog("Max", 5, 30)
-
-print(dog1.bark())
-print(dog2.run_speed())
-print(dog1.fight(dog2))
-print(dog3.fight(dog1))
 
 # Exercise 3: Dogs Domesticated
 
---
+++ Week3/Day2/ExerciseXP/dog.py
@@ -0,0 +1,22 @@
+class Dog:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def bark(self):
+        return f"{self.name} is barking"
+
+    def run_speed(self):
+        return self.weight / self.age * 10
+
+    def fight(self, other_dog):
+        my_power = self.run_speed() * self.weight
+        other_power = other_dog.run_speed() * other_dog.weight
+
+        if my_power > other_power:
+            return f"{self.name} won the fight"
+        elif other_power > my_power:
+            return f"{other_dog.name} won the fight"
+        else:
+            return "It's a tie"
